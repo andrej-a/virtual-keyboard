d69f5fd17217b75fee2b30d0b84f960b4306f4d2
github-usernameandrej-a <amelyanovich.andrej@mail.ru>
2022-05-04 10:42:32 +0300

    feat(buttons): add getting information from localStorage for first initialization

--- src/js/components/buttons.js
+++ src/js/components/buttons.js
@@ -8,14 +8,26 @@ export default class CreateButtons {
   }
 
   init(item) {
+    const {
+      key, keyEN_SHIFT, keyRU, keyRU_SHIFT, code,
+    } = item; // get props from BD
+
     this.button = new CreateElement('button').init();
+
     if (item.system) {
       this.button.classList.add(this.systemButtonClassName);
     } else {
       this.button.classList.add(this.buttonClassName);
     }
-    this.button.code = item.code;
-    this.button.innerText = item.key;
+    this.button.code = code; // put the btn code to button
+
+    if (!localStorage.getItem('language')) { // check language in localStorage
+      localStorage.setItem('language', 'EN'); // if is not set English like default language
+      this.button.innerText = key;
+    } else {
+      this.button.innerText = localStorage.getItem('language') === 'EN' ? key : keyRU; // if it is choose between EN and RU
+    }
+
     return this.button;
   }
 }
